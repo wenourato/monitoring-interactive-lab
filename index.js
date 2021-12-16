f0788fb9616ad280c8be88c813040831f5110378
Wyatt Enourato <wenourato@gmail.com>
2021-12-16 11:24:52 -0700

    monitoring code along

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+const port = process.env.PORT || 4545
+
+app.listen(port, () => console.log(`Take us to ward 4545 ${port}!`))
